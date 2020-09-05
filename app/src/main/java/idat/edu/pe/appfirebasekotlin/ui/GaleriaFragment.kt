8ad0e83eef4f0602db75f8d4dcfbea484677760d
LuisAngel <luisangel.salvat@gmail.com>
2020-09-05 18:37:21 -0500

    segundo cambio

--- app/src/main/java/idat/edu/pe/appfirebasekotlin/ui/GaleriaFragment.kt
+++ app/src/main/java/idat/edu/pe/appfirebasekotlin/ui/GaleriaFragment.kt
@@ -27,7 +27,7 @@ class GaleriaFragment : Fragment() {
         // Inflate the layout for this fragment
         val view : View = inflater.inflate(R.layout.fragment_galeria, container, false)
         rvimagenes = view.findViewById(R.id.rvimagenes)
-        ListarImagenes(view)
+        //ListarImagenes(view)
         return view
     }
 
--- app/src/main/java/idat/edu/pe/appfirebasekotlin/ui/ListaFragment.kt
+++ app/src/main/java/idat/edu/pe/appfirebasekotlin/ui/ListaFragment.kt
@@ -6,8 +6,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.firestore.DocumentChange
 import com.google.firebase.firestore.FirebaseFirestore
 import idat.edu.pe.appfirebasekotlin.R
 import idat.edu.pe.appfirebasekotlin.adapter.ImagenAdapter
@@ -30,7 +32,7 @@ class ListaFragment : Fragment() {
         val lstpersonas : ArrayList<Persona> = ArrayList()
         rvfirestore = view.findViewById(R.id.rvfirestore)
         firestoredb = FirebaseFirestore.getInstance()
-        firestoredb.collection("Persona")
+        /*firestoredb.collection("Persona")
             .get()
             .addOnSuccessListener { result ->
                 for (document in result) {
@@ -45,7 +47,25 @@ class ListaFragment : Fragment() {
             .addOnCompleteListener {
                 rvfirestore.adapter = PersonaAdapter(lstpersonas, view.context)
                 rvfirestore.layoutManager = LinearLayoutManager(view.context)
+            }*/
+        firestoredb.collection("Persona")
+            .addSnapshotListener { snapshots, e ->
+                if (e != null) {
+                   Toast.makeText(context, "Error", Toast.LENGTH_LONG).show()
+                }
+                for (dc in snapshots!!.documentChanges) {
+                    if (dc.type == DocumentChange.Type.ADDED) {
+                        lstpersonas.add(Persona(dc.document.data["nombre"].toString(),
+                            dc.document.data["apellido"].toString(),
+                            dc.document.data["edad"].toString().toInt()))
+                        //Log.d("FIRE", "New city: ${dc.document.data["apellido"]}")
+                    }
+
+                }
+                rvfirestore.adapter = PersonaAdapter(lstpersonas, view.context)
+                rvfirestore.layoutManager = LinearLayoutManager(view.context)
             }
+
         return view
     }
 
